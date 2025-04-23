d1bbb527429972e2eb1f7d573252e040b623c0ea
kimkhanh151003 <kimkhanh151003@gmail.com>
2025-04-23 01:39:51 +0000

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,44 @@
-// import React from 'react';
-// import './Contact.css';
-// import galaxyImage from './images/galaxy.jpg';
-
-// function Contact() {
-//   return (
-//     <div className="contact-container">
-//       <h1>CONTACT US</h1>
-//       <div className="content-row">
-//         <div className="text-content">
-//           <p>Liên hệ với chúng tôi!</p>
-//           <h2>Thông tin liên lạc</h2>
-//           <ul>
-//             <li>Email: kimkhanh151003@gmail.com</li>
-//             <li>Điện thoại: 0376117757</li>
-//             <li>Địa chỉ: 123 Tố Hữu, Quận Thuận hoá, TP.Huế</li>
-//           </ul>
-//         </div>
-//         <div class="image">
-//             <a><img src={galaxyImage} alt="Galaxy" className="page-image" /></a>
-//         </div>
-        
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Contact;
-
-
 import React from 'react';
 import './Contact.css';
 import runningIcon from './images/running-icon.png';
 import phoneIcon from './images/phone-icon.png';
 import locationIcon from './images/location-icon.png';
 
+
 function Contact() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        const formData = new FormData(event.target);
+        const data = {
+          email: formData.get('email'),
+          name: formData.get('name'),
+          message: formData.get('message')
+        };
+    
+        fetch('https://api.emailjs.com/api/v1.0/email/send', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            service_id: 'service_291q39r',
+            template_id: 'template_l2g40o9',
+            user_id: 'gUfQJ-89n1vRz-9jB',
+            template_params:{...data, to_email:'kimkhanh151003@gmail.com'}
+          })
+        })
+      };
   return (
     <div className="contact-container">
       <div className="contact-form-section">
         <h1>Contact Us</h1>
         <p>Any questions or remarks? Just write us a message!</p>
-        <div className="contact-form">
-          <input type="email" placeholder="Enter a valid email address" />
-          <input type="text" placeholder="Enter your Name" />
-          <button type="submit">SUBMIT</button>
-        </div>
+        <form className="contact-form" onSubmit={handleSubmit}>
+            <input type="email" name="email" placeholder="Enter a valid email address" required/>
+            <input type="text" name="name" placeholder="Enter your Name" required/>
+            <button type="submit">SUBMIT</button>
+        </form>
       </div>
       <div className="contact-info-section">
         <div className="info-card">
