31031de2009ca0bd4ee53ce266b3c55c1d3fb1be
klenhs03 <kimkhanh151003@gmail.com>
2025-05-12 15:54:49 +0700

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -88,6 +88,18 @@ function Contact() {
           <h3>OUR OFFICE LOCATION</h3>
           <p>Kew Company<br />Thuan Hoa District, Hue City, Vietnam</p>
         </div>
+        <div className="info-card">
+          <h2>OUR LOCATION ON MAP</h2>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3826.8883598422847!2d107.5901389!3d16.4591389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTbCsDI3JzMyLjkiTiAxMDfCsDM1JzI0LjUiRQ!5e0!3m2!1sen!2s!4v1698765432109!5m2!1sen!2s"
+            width="100%"
+            height="500"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            title="Google Maps Location"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
